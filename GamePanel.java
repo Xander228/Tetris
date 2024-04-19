226119cc85ea39cb7b810539fc26a33be306b282
PatribotsProgramming <78040067+Xander228@users.noreply.github.com>
2024-04-19 16:43:25 -0700

    Added some coments

--- GamePanel.java
+++ GamePanel.java
@@ -4,117 +4,134 @@ import java.util.HashMap;
 
 public class GamePanel extends JPanel {
 
+    //Creates an enum with a list of all game states
     private enum GameStates {
-        GENERATION_PHASE (0),
-        FALLING_PHASE (1),
-        LOCK_PHASE (2),
-        CLEAR_PHASE (3),
-        UPDATE_PHASE (4),
-        FINISHED_PHASE (5);
+        GENERATION_PHASE (0), //Initialize the tetromino object for play
+        FALLING_PHASE (1), //Drop the teromino and allow user input
+        LOCK_PHASE (2), //Decides whether the tetromino should lock 
+        UPDATE_PHASE(3), //Writes tetromino to board
+        CLEAR_PHASE(4), //Clears lines & tallies points, lines, and levels
+        FINISHED_PHASE (5); //Ends the game
 
-        private final int type;
-        GameStates(int typeAsInt) {
-            this.type = typeAsInt;
-        }
-
-        public int toInt() {
-            return this.type;
+        //GameStates constructor
+        GameStates(int typeAsInt){
         }
     }
 
-    private GameStates gameState;
+    private GameStates gameState; //Declare the gameStates
+    private MatrixPanel matrixPanel; //Declare the matrixPanel
+    private PiecePanel piecePanel; //Declare the piecePanel
+    private boolean hasSwap; //Declare the hasSwap variable
 
-    private MatrixPanel matrixPanel;
-    private PiecePanel piecePanel;
-    private boolean hasSwap;
+    private int score; //Declare the score variable
+    private int lines; //Declare the lines variable
+    private int level; //Declare the level variable
 
-    private int score;
-    private int lines;
-    private int level;
-
+    //GamePanel constructor
     public GamePanel() {
-        this.gameState = GameStates.GENERATION_PHASE;
 
-        // Create an instance of the TetrisBoard class
-        matrixPanel = new MatrixPanel();
-        // Create an instance of the ScorePanel class
-        piecePanel = new PiecePanel();
+        this.gameState = GameStates.GENERATION_PHASE; //Sets the initial game state to the first phase, GENERATION_PHASE
 
-        setBorder(BorderFactory.createMatteBorder(10, 10, 10, 10, Constants.PRIMARY_COLOR));
-        setBackground(Constants.PRIMARY_COLOR);
-        setLayout(new BorderLayout(10, 10));
+        matrixPanel = new MatrixPanel(); //Create the MatrixPanel object
+        piecePanel = new PiecePanel(); //Create the PiecePanel object
+
+        //Set up the panel properties
+        setBorder(BorderFactory.createMatteBorder(10, 10, 10, 10, Constants.PRIMARY_COLOR)); //Add a border around the frame
+        setBackground(Constants.PRIMARY_COLOR); //Set the background color of the panel
+        setLayout(new BorderLayout(10, 10)); //Sets the edge offset of member panels to properly space them
 
         // Add the matrixPanel and piecePanel to the panel
-        add(matrixPanel);
-        add(piecePanel, BorderLayout.EAST);
+        add(matrixPanel); //Adds the matrixPanel object to the center of the parent panel
+        add(piecePanel, BorderLayout.EAST); //Adds the piecePanel object to the right of the parent panel
 
-        score = 0;
-        lines = 0;
-        level = 1;
-
-        this.hasSwap = false;
+        score = 0; //Initializes the score to 0
+        lines = 0; //Initializes the lines to 0
+        level = 1; //Initializes the level to 1
+        this.hasSwap = false; //Initializes the hasSwap to false
     }
 
-    public int[] getScores(){
-        return new int[]{score, lines, level};
-    }
+    //The run method is called every game loop by the timer in the TetrisFrame
+    //It accepts the map of key times for user input
     public boolean run(HashMap<Integer, Integer> keyPressed) {
+
+        //switch statement to decide what code to run based on current game state
         switch(this.gameState){
+
             case GENERATION_PHASE:
                 //Set tetromino to the piece passed in, sets start location
                 //If hold check is true switches hold with current piece
-                //Continues to FALLING_PHASE
-                Tetromino newPiece;
-                if(hasSwap) {
-                    //swap with hold
-                    newPiece = piecePanel.switchPiece(matrixPanel.getPiece());
-                    if(newPiece == null) newPiece = piecePanel.getNewPiece();
+                //Continues to FALLING_PHASE at the end
+
+                Tetromino newPiece; //Declare local variable newPiece to store the new tetromino object
+
+                //If hasSwap is true, that means that a piece was requested to be held, and it should execute the code to run swap them, otherwise pull a new piece
+                 if(hasSwap) {
+                    //swap current piece with hold piece
+                    newPiece = piecePanel.switchPiece(matrixPanel.getPiece()); //Set the hold piece to the current piece and the current piece to the held piece
+                    if(newPiece == null) newPiece = piecePanel.getNewPiece(); //If it has received a null held piece, then it pull the next piece from queue
                 } else {
-                    //pull from queue
-                    newPiece = piecePanel.getNewPiece();
+                    //pull piece from queue
+                    newPiece = piecePanel.getNewPiece(); //Sets the newPiece to a piece pulled from queue
                 }
-                this.matrixPanel.setPiece(newPiece);
-                this.gameState = GameStates.FALLING_PHASE;
-                break;
+                this.matrixPanel.setPiece(newPiece); //Sets the active piece in matrixPanel to the newPiece
+                this.gameState = GameStates.FALLING_PHASE; //Sets the next game phase to FALLING_PHASE
+                break; //Breaks out of the current game phase
 
             case FALLING_PHASE:
-                //Allow user input to move piece right, left, rotate, and drop; also drops each x seconds based on level
-                //If hold is pressed, set hold check true and return to GENERATION_PHASE
-                //checks for piece underneath current tetromino and sets state to LOCK_PHASE if true
-                //Hard drop skips to CLEAR_PHASE
+                //Allow user input to move piece right, left, rotate, and drop
+                //If hold is pressed, set hasSwap true and return to GENERATION_PHASE
+                //Hard drop skips immediately to UPDATE_PHASE after dropping the piece
+                //Checks for piece underneath current tetromino and sets state to LOCK_PHASE if true
+
+                //If the hold key is pressed and a swap has not been requested already then swap pieces
+                //If hasSwap is true, that means a piece has been held since the last lock, and it can't switch again
+                //Checks if the key time equals 1 because this should only be triggered once per keypress
                 if (keyPressed.get(Constants.HOLD_KEY) == 1 && !hasSwap) {
-                    this.hasSwap = true;
-                    this.gameState = GameStates.GENERATION_PHASE;
-                    break;
+                    this.hasSwap = true; //Preps the GENERATION_PHASE for a swap and prevents additional holds before a lock
+                    this.gameState = GameStates.GENERATION_PHASE; //Sets the next game phase to GENERATION_PHASE
+                    break; //Breaks out of the current game phase
                 }
+
+                //If the hard drop key is pressed the hard drop the piece
+                //Checks if the key time equals 1 because this should only be triggered once per keypress
                 if(keyPressed.get(Constants.HARD_DROP_KEY) == 1) {
-                    score += 2 * matrixPanel.hardDrop();
-                    this.gameState = GameStates.CLEAR_PHASE;
+                    score += 2 * matrixPanel.hardDrop(); //Runs hardDrop which drops the piece and returns the number of lines dropped to award points
+                    this.gameState = GameStates.UPDATE_PHASE; //Sets the next game phase to UPDATE_PHASE
+                    break; //Breaks out of the current game phase
                 }
 
-                matrixPanel.handleKeyPress(keyPressed);
+                matrixPanel.handleKeyPress(keyPressed); //Passes in pressed keys to matrixPanel to handle right, left, rotate, and drop movement
 
+                //Determines whether the fall time has elapsed based on current level
                 if(matrixPanel.shouldFall(level)){
+                    //Attempts to drop the piece and returns true if successful
                     if(matrixPanel.drop()){
-                        if(matrixPanel.isSoftDropping()) score++;
+                        if(matrixPanel.isSoftDropping()) score++; //Adds a bonus score of 1 per line if soft dropping
                     } else {
+                        //If the piece did not drop then reset lock timers and continue to lock phase
+
+                        //If the piece has fallen below its previous lock point, lock counter and timer are reset
                         if(matrixPanel.canResetCounter()) {
-                            matrixPanel.resetTimer();
-                            matrixPanel.resetCounter();
+                            matrixPanel.resetTimer(); //Resets the timer that determines when to lock the piece
+                            matrixPanel.resetCounter(); //Resets the counter that determines how many times the timer can be reset
                         }
-                        this.gameState = GameStates.LOCK_PHASE;
-                        break;
+                        this.gameState = GameStates.LOCK_PHASE; //Sets the next game phase to LOCK_PHASE
+                        break; //Breaks out of the current game phase
                     }
                 }
 
-                matrixPanel.repaint();
-                break;
+                piecePanel.updateScores(score,lines,level); //Updates the score panel with new scores
+                repaint(); //Redraw everything because things have moved
+                break; //Breaks out of the current game phase
 
             case LOCK_PHASE:
-                //Allow user input to move piece right, left, rotate, and lock; force locks after x seconds
-                //If hold is pressed, set hold check true and return to GENERATION_PHASE
+                //Allow user input to move piece right, left, rotate, and drop
+                //If hold is pressed, set hasSwap true and return to GENERATION_PHASE
+                //Hard drop skips immediately to UPDATE_PHASE
                 //If piece is now able to drop revert to FALLING_PHASE
-                //If locked continue to CLEAR_PHASE
+                //If locked continue to UPDATE_PHASE
+
+
                 matrixPanel.setLowestLock();
 
                 if (keyPressed.get(Constants.HOLD_KEY) == 1 && !hasSwap) {
@@ -124,39 +141,40 @@ public class GamePanel extends JPanel {
                 }
                 if(keyPressed.get(Constants.HARD_DROP_KEY) == 1) {
                     score += 2 * matrixPanel.hardDrop();
-                    this.gameState = GameStates.CLEAR_PHASE;
+                    this.gameState = GameStates.UPDATE_PHASE;
                     break;
                 }
 
                 if(matrixPanel.handleKeyPress(keyPressed)){
                     if(matrixPanel.drop()){
-                        //score++
+                        score++;
                         this.gameState = GameStates.FALLING_PHASE;
                     } else if(matrixPanel.canResetTimer())
                         matrixPanel.resetTimer();
                 }
 
                 if(matrixPanel.isTimerElapsed()){
-                    this.gameState = GameStates.CLEAR_PHASE;
+                    this.gameState = GameStates.UPDATE_PHASE;
                     break;
                 }
 
                 matrixPanel.incrementTimer();
-                matrixPanel.repaint();
-                break;
-
-            case CLEAR_PHASE:
-                //Writes current tetromino to its location on the board
-                //Continues to UPDATE_PHASE
-                matrixPanel.lockTetromino();
-                this.hasSwap = false;
-                this.gameState = GameStates.UPDATE_PHASE;
+                piecePanel.updateScores(score,lines,level);
+                repaint();
                 break;
 
             case UPDATE_PHASE:
+                //Writes current tetromino to its location on the board
+                //Continues to CLEAR_PHASE
+                matrixPanel.lockTetromino();
+                this.hasSwap = false;
+                this.gameState = GameStates.CLEAR_PHASE;
+                break;
+
+            case CLEAR_PHASE:
 
                 //checks for lines & clears lines and tallies them
-                //Continues to UPDATE_PHASE
+                //Continues to GENERATION_PHASE
                 int clearedLines = 0;
                 if (matrixPanel.identifyRows()) {
                     matrixPanel.repaint();
@@ -197,5 +215,9 @@ public class GamePanel extends JPanel {
 
         }
         return true;
+    }
+
+    public int[] getScores(){
+        return new int[]{score, lines, level};
     }
 }
--- TetrisFrame.java
+++ TetrisFrame.java
@@ -4,89 +4,102 @@ import java.awt.event.*;
 import java.util.HashMap;
 
 public class TetrisFrame extends JFrame {
-    private GamePanel gamePanel;
-    private Timer timer;
+    private GamePanel gamePanel; //Declare the gamePanel
 
-    
+    private Timer timer; //Declare to timer
+
+    //TetrisFrame constructor
     public TetrisFrame() {
-        // Set up the frame properties
-        setTitle("Tetris");
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setResizable(false);
+        //Set up the frame properties
+        setTitle("Tetris"); //Title of the frame
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //Behavior on close, exits the program when the frame is closed
+        setResizable(false); //sets the frame to a fixed size, not resizeable by a user
 
-        //menuLoop is a panel within the frame that contains all screenPanels
-        //menuLoop switches the current panel based on the screenStates
-        gamePanel = new GamePanel();
-        add(gamePanel);
-        pack();
+        //GamePanel is a panel within the frame that contains sub panels related to gameplay
+        gamePanel = new GamePanel(); //Create new gamePanel object
+        add(gamePanel); //Adds the gamePanel object to the frame
+        pack(); //Resizes the frame to fit the gamePanel
         
-        //create a hashmap that maps a key to a true false value to represent weather or not its pressed
-        HashMap<Integer, Boolean> keyPressed = new HashMap<Integer, Boolean>();
-        HashMap<Integer, Integer> keyTimes = new HashMap<Integer, Integer>();
+        //Create a hashmap that maps a key to a true false value to represent weather or not its pressed
+        HashMap<Integer, Boolean> keyPressed = new HashMap<Integer, Boolean>(); //Map of currently pressed keys
+        HashMap<Integer, Integer> keyTimes = new HashMap<Integer, Integer>(); //Map of the amount of time, in code loops, that a key has been pressed
 
-        //adds each value of keyList to the hashmap
-        for(int key : Constants.KEY_LIST) keyPressed.put(key, false);
-        for(int key : Constants.KEY_LIST) keyTimes.put(key, 0);
+        //Adds each key, with its default value, of keyList to the hashmaps
+        for(int key : Constants.KEY_LIST) keyPressed.put(key, false); //Creates a key for each index in KEY_LIST
+        for(int key : Constants.KEY_LIST) keyTimes.put(key, 0); //Creates a key for each index in KEY_LIST
 
-        // Set up KeyListener for user input
-        addKeyListener(new KeyListener() {
-            @Override
-            public void keyTyped(KeyEvent e) {
-            }
-
-            @Override
-            public void keyPressed(KeyEvent e) {
-                keyPressed.replace(e.getKeyCode(), true);
-            }
-
-            @Override
-            public void keyReleased(KeyEvent e) {
-                keyPressed.replace(e.getKeyCode(), false);
-            }
-        });
-
-        // Set up a Timer for the game loop
-        timer = new Timer(Constants.LOOP_TIME, new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                for(int key : Constants.KEY_LIST) {
-                    if(keyPressed.get(key)) keyTimes.replace(key,1 + keyTimes.get(key));
-                    else keyTimes.replace(key, 0);
+        //Set up KeyListener to accept user input though key presses
+        //Adds the KeyListener defined below to the frame
+        addKeyListener(
+            //creates a new KeyListener object with the following methods overriden
+            new KeyListener() {
+                //Must be overriden to create object due to KeyListener being an interface
+                @Override
+                public void keyTyped(KeyEvent e) {
                 }
 
-                //pass in the list of currently pressed keys when running the loop
-                if(!gamePanel.run(keyTimes)) gameOver();
+                //Overrides the keyPressed method to add the state of the current pressed key to the map
+                @Override
+                public void keyPressed(KeyEvent e) {
+                    keyPressed.replace(e.getKeyCode(), true); //receives the pressed key and replaces the existing value in the map with true
+
+                }
+
+                //Overrides the keyReleased method to add the state of the current released key to the map
+                @Override
+                public void keyReleased(KeyEvent e) {
+                    keyPressed.replace(e.getKeyCode(), false); //receives the released key and replaces the existing value in the map with false
+                }
             }
-        });
-        // Set the frame focusable for KeyListener
+        );
+
+        //Set up a Timer to run the game loop
+        //Creates a new timer object with a delay equal to LOOP_TIME which is the time it takes for one loop
+        timer = new Timer(Constants.LOOP_TIME,
+            //Creates a new ActionListener object with the following methods overriden
+            new ActionListener() {
+                //Overrides the actionPerformed method which is called every time the LOOP_TIME elapses
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    //Every loop, key times are incremented as long as the key is still pressed
+                    for(int key : Constants.KEY_LIST) {
+                        if(keyPressed.get(key)) keyTimes.replace(key,1 + keyTimes.get(key)); //If the key is still being held, increase the time
+                        else keyTimes.replace(key, 0); //If the key has been released then reset the timer to 0
+                    }
+
+                    //Runs the gamePanel's run method and passes in the map of key times
+                    //If the gamePanel fails to return true, it means the game is over and the gameOver method is called
+                    if(!gamePanel.run(keyTimes)) gameOver();
+                }
+            }
+        );
+        //Set the frame focusable for KeyListener, allowing it to accept key inputs when in focus
         setFocusable(true);
 
-        // Start the timer
+        //Start the timer
         timer.start();
 
-        // Set the frame visible
+        //Set the frame visible
         setVisible(true);
     }
+
+    //Gets the scores and creates a game over object to display the GameOverPanel, then stops the game timer
     private void gameOver(){
-        int[] scores = gamePanel.getScores();
-        new GameOver(this, scores[0], scores[1], scores[2]);
-        timer.stop();
-    }
-    public void startNewGame(){
-        this.remove(gamePanel);
-        gamePanel = new GamePanel();
-        this.add(gamePanel);
-        pack();
-        timer.start();
+        int[] scores = gamePanel.getScores(); //Gets an array of the scores from the gamePanel
+        new GameOver(this, scores[0], scores[1], scores[2]); //Creates the GameOver object which displays the scores and end options
+        timer.stop(); //Stops the game timer to stop calls to this method
     }
 
+    //Replaces old gamePanel with a new instance, then restarts the timer
+    public void startNewGame(){
+        gamePanel = new GamePanel(); //Replaces the existing gamePanel object with the new one
+        this.add(gamePanel); //Re-adds the new gamePanel object to the frame
+        pack(); //Ensures that the new gamePanel is at its desired size and removes old panel
+        timer.start(); //Restarts the game loop
+    }
+
+    //Main method
     public static void main(String[] args) {
-        // Run the application on the EDT
-        SwingUtilities.invokeLater(new Runnable() {
-            @Override
-            public void run() {
-                new TetrisFrame();
-            }
-        });
+        new TetrisFrame(); //Create new TetrisFrame object
     }
 }
